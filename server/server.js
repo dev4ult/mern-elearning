774258e8db49a36ec6986c022c467c9cce284616
dev4ult <nibrasbiasaaja@gmail.com>
2023-03-05 18:04:40 +0700

    testing the server to listen a request

--- server/server.js
+++ server/server.js
@@ -1,3 +1,30 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
+import bodyParser from 'body-parser';
 import logger from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+const app = express();
+
+// MIDDLEWARE CONFIGURATION
+
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(cors());
+
+// HTTP REQUEST AND RESPOND
+
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'hello it is working fine' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
